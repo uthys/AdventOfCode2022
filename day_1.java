8c8d06cdb910b483844d4ff2ef8bb2ad4c3c7679
uthys <octagos1@gmail.com>
2022-12-12 19:26:11 +0200

    day1 fix

--- day_1.java
+++ day_1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class day_1 {
@@ -20,6 +21,11 @@ public class day_1 {
                 cals.add(item);
             }
             reader.close();
+            //
+            //
+            //PART 1
+            ArrayList<Integer> elves = new ArrayList<>();
+            int CurrentElf = 0;
             int count = 0;
             int max = 0;
             for(String s : cals){
@@ -29,6 +35,24 @@ public class day_1 {
                     if(count > max) max = count;}
                 catch(NumberFormatException ex){count=0; continue;}
             }
+            //
+            //
+            //PART 2
+            for(String s : cals){
+                try{
+                    CurrentElf += Integer.parseInt(s);
+                }catch(NumberFormatException ex){
+                    elves.add(CurrentElf);
+                    CurrentElf = 0;
+                    continue;
+                }
+            }
+            Collections.sort(elves);
+            int sum = 0;
+            for(int i=elves.size()-3; i < elves.size(); i++){
+                sum += elves.get(i);
+            }
+            System.out.println(sum);
         
         }catch(FileNotFoundException e) {System.out.println(e);}
         
